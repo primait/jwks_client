980b2851807cc10cf296c51218bbf899540aa34b
Simone Cottini <cottini.simone@gmail.com>
2021-10-29 15:07:26 +0200

    Remove broken example code

--- examples/get_jwks.rs
+++ examples/get_jwks.rs
@@ -16,13 +16,9 @@ async fn main() {
 
     let url: Url = Url::from_str(url_string.as_str()).unwrap();
     let url: Url = url.join(".well-known/jwks.json").unwrap();
-    assert_eq!(
-        url.as_str(),
-        format!("{}/.well-known/jwks.json", url_string)
-    );
 
     let source: WebSource = WebSource::new(url);
-    let client: JwksClient = JwksClient::new(source);
+    let client: JwksClient<WebSource> = JwksClient::new(source);
 
     // The kid "unknown" cannot be a JWKS valid KID. This must not be found here
     let result: Result<JsonWebKey, JwksClientError> = client.get("unknown".to_string()).await;
@@ -34,7 +30,4 @@ async fn main() {
     // The provided kid (assuming is the same you got from your tenant) is valid and could be found.
     let result: Result<JsonWebKey, JwksClientError> = client.get(kid.clone()).await;
     println!("Get with kid \"{}\": {:?}", kid, result.unwrap());
-
-    let result: Result<Claims, JwksClientError> =
-        client.decode::<Claims>("token", "audience").await;
 }
